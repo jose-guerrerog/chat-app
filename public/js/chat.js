38890c14dc268c6951475aed19ac6ef195030ef4
jose-guerrerog <joseguerrerog880@gmail.com>
2019-09-04 13:16:30 +1000

    Displaying user messages with time

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,35 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = document.querySelector('input')
+const $messageFormButton = document.querySelector('button')
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    
     const message = e.target.elements.message.value
 
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
-const filter = require('bad-words')
+const Filter = require('bad-words')
+const { generateMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -18,21 +19,21 @@ let count = 0
 io.on('connection', (socket) => {
 	console.log('New WebSocket connection')
 	
-	socket.emit('message', 'Welcome!')
-	socket.broadcast.emit('message', 'A new user has joined!')
+	socket.emit('message', generateMessage('Welcome!'))
+	socket.broadcast.emit('message', generateMessage('A new user has joined!'))
 
 	socket.on('sendMessage', (message, callback) => {
-		const filter = new filter()
+		const filter = new Filter()
 
 		if (filter.isProfane(message)) {
 			return callback('Profanity is not allowed')
 		}
-		io.emit('message', message)
+		io.emit('message', generateMessage(message))
 		callback()
 	})
 
 	socket.on('disconnect', () => {
-		io.emit('message', 'A user has left')
+		io.emit('message', generateMessage('A user has left'))
 	})
 })
 
