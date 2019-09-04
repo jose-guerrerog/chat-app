4089e25da8b2215d5004c54b5c9b31a16013cb83
jose-guerrerog <joseguerrerog880@gmail.com>
2019-09-04 17:05:00 +1000

    Working with rooms and add styling

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,9 +9,13 @@ const $messages = document.querySelector('#messages')
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
+        username: message.username,
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
@@ -35,4 +39,11 @@ $messageForm.addEventListener('submit', (e) => {
         }
         console.log('The message was delivered!')
     })
+})
+
+socket.emit('join', {username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
 })
