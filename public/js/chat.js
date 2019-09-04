7ab13d3a328da56a788d8120bac1c4030b1253bf
jose-guerrerog <joseguerrerog880@gmail.com>
2019-09-04 12:07:46 +1000

    Implementing Event acknowledgements

---
+++ public/js/chat.js
@@ -0,0 +1,18 @@
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log('The message was delivered!')
+    })
+})
